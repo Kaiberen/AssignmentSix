c2e793530c993196298faf45348b05d307f155f0
Kaiberenn <Kaiberen@hawaii.edu>
2019-02-28 09:15:39 -1000

    Part one through seven complete

--- AssignmentSix/ViewController.swift
+++ AssignmentSix/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var displayTextLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   self.displayTextLabel.text=""
     }
-
-
+    
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+        var userText: String!
+        
+        userText = self.textField.text
+        self.displayTextLabel.text=userText
+    }
+    
 }
-
